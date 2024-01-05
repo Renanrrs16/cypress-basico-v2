de330928803ac104e430b530febab281d86a904a
Renan Souza <renanrs16@gmail.com>
2024-01-05 08:28:52 -0400

    Break the app on purpose

--- cypress/integration/Privacy.spec.js
+++ cypress/integration/Privacy.spec.js
@@ -1,3 +1,4 @@
-it('testa a página da política de privacidade de forma independente', function(){
-cy.visit('/src/privacy.html').contains('Talking About Testing').should('be.visible')
+it.only('testa a página da política de privacidade de forma independente',function(){
+    cy.visit('./src/privacy.html')
+    cy.contains('a aplicação é um exemplo').should('be.visible')
 })
